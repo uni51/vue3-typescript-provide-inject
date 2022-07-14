33f9c0cf89e8708007414087caa66e1147891b88
Koichi.Shimomura <unieye51@gmail.com>
2022-07-14 16:11:59 +0900

    9-50 Provide / Inject

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue';
+import { provide } from 'vue'
+import { todos } from './useTodo'
+
+provide('todos', todos)
+</script>
+
+<template>
+  <TodoList />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
